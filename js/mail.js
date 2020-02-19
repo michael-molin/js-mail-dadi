410d184d61072a5f8e9e75e6bfeeee07e510128f
Michael <m.molin.1987@gmail.com>
2020-02-19 14:15:22 +0100

    Creazione esercizio mail

---
+++ js/mail.js
@@ -0,0 +1,14 @@
+var arrayMail = ['pippo@gmail.com' , 'ziopaperone@gmail.com' , 'paperina@gmail.com' , 'paperino@gmail.com' , 'topolino@gmail.com']
+var mailRichiesta = prompt('Inserisci l\'indirizzo mail che stai cercando: ');
+
+for (var i=0 ; i <= arrayMail.length ; i++  ) {
+    if (mailRichiesta == arrayMail[i]) {
+        var mailPosizione = parseInt(i);
+    } else {}
+}
+
+if (!isNaN(mailPosizione)) {
+    console.log(mailRichiesta + ' trovata! In posizione: ' + parseInt(mailPosizione + 1) );
+} else {
+    console.log(mailRichiesta + ' non presente nell\'Array');
+}
